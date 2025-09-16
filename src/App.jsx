ec9b92a26d2e0d92341de83d7b3cfcdbdfa585dd
jonathanvra <jonathan.vra@gmail.com>
2025-09-16 10:25:01 -0500

    chore: Se instala tailwind y se elimina lo no necesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='grid grid-cols-3 grid-rows-2 gap-3 h-[504px] w-[1176px]'>
+      <div className='bg-red-500'>1</div>
+      <div className='bg-red-500'>2</div>
+      <div className='col-start-1 row-start-2 bg-red-500'>3</div>
+      <div className='col-start-2 row-start-2 bg-red-500'>4</div>
+      <div className='row-span-2 col-start-3 row-start-1 bg-red-500'>5</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
