255b6804916a1b1179c58cfd4144560c059d5175
jonathanvra <jonathan.vra@gmail.com>
2025-09-17 08:11:56 -0500

    fix: Manejo correcto de rutas SPA

--- src/components/staticUnits.jsx
+++ src/components/staticUnits.jsx
@@ -1,10 +1,17 @@
 import CanalClima from './canalClima';
+import { Link } from 'react-router-dom';
 
 export default function staticUnits() {
   return (
     <div>
       <div className='grid grid-cols-3 grid-rows-2 gap-3 h-[504px] w-[1176px]'>
-        <div className='bg-red-500'>1</div>
+        <div className='bg-red-500'>
+          <Link to='/dinamicas'>
+            <button className='w-full h-full text-white text-2xl font-bold cursor-pointer'>
+              Ir a Dinámicas
+            </button>
+          </Link>
+        </div>
         <div className='bg-red-500'>2</div>
         <div className='col-start-1 row-start-2 bg-red-500'>3</div>
         <div className='col-start-2 row-start-2 bg-red-500'>4</div>
